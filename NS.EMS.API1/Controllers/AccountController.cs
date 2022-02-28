322d1acf8ea1a9b0e9dd8736417e0e7165bfb195
Ankita Lande <97441450+Ankita-Lande17@users.noreply.github.com>
2022-02-28 23:21:06 +0530

    WEB-API-SWAGGER Execute successfully

---
+++ NS.EMS.API1/Controllers/AccountController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using NS.EMS.API1.Entities;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+
+namespace NS.EMS.API1.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public AccountController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpPost]
+        public IActionResult Login(Login login)
+        {
+            using(var contect = new EmployeeDBContext())
+            {
+                var user = contect.Login.Where(x => x.UserName == login.UserName && x.Password==login.Password && x.IsActive==true).FirstOrDefault();
+                if(user!=null)
+                {
+                    var token = GenerateToken(user.UserName, user.Role);
+                    return Ok(token);
+                }
+                else
+                {
+                    return Ok("Invalid Username or Password.");
+                }
+            }
+            return Ok();
+        }
+        private string GenerateToken(string username,string role)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+            var claims = new Claim[] { new Claim (ClaimTypes.Name, username), new Claim (ClaimTypes.Role, role) };
+            var token = new JwtSecurityToken(
+                _configuration["Jwt:Issuer"],
+                _configuration["Jwt:Audience"],
+                claims,
+                expires:System.DateTime.Now.AddMinutes(60),
+                signingCredentials:credentials
+                );
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
---
+++ NS.EMS.API1/Controllers/EmployeeController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NS.EMS.API1.Entities;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NS.EMS.API1.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    [Authorize]
+    public class EmployeeController : ControllerBase
+    { 
+
+       
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult GetEmployees()
+        {
+            var employeeList = new List<Employee>();
+            using (var context = new EmployeeDBContext())
+            {
+                employeeList= context.Employee.ToList();
+
+            }
+            return Ok(employeeList);
+        }
+        [Authorize]
+        [HttpGet]
+        public IActionResult GetEmployeeByEid(int eId)
+        {
+            var employee = new Employee();
+            using (var context = new EmployeeDBContext())
+            {
+              employee = context.Employee.Find(eId);  
+          }
+            if(employee==null)
+            {
+                return NotFound("Employee Not found");
+            }
+            return Ok(employee);
+        }
+        [Authorize]
+        [HttpPost]
+        public IActionResult AddEmployee(Employee employee)
+        {
+            using (var context = new EmployeeDBContext())
+            {
+                context.Employee.Add(employee);
+                context.SaveChanges();
+
+            }
+            return Ok("Record Inserted Successfully");
+        }
+
+        [Authorize(Roles ="Admin")]
+        [HttpPut]
+
+        public IActionResult UpdateEmployee(Employee employee)
+        {
+            using (var context = new EmployeeDBContext())
+            {
+                context.Employee.Update(employee);
+                context.SaveChanges();
+
+            }
+            return Ok("Record Update Successfully");
+        }
+    }
+
+}
